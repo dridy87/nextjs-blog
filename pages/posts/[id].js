1adcef8aaa42f0f073778bafdc13c65bad9793ef
dridy87 <dridy87@gamil.com>
2020-12-16 15:59:50 +0900

    test

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -13,13 +13,13 @@ export default function Post({ postData }) {
  
       {/* {postData.title} */}
       <br />
-      {postData.id}
+      {/* {postData.id} */}
       <br />
-      {postData.date}
+      {/* {postData.date} */}
       <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      {/* <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
 
-       <Date dateString={postData.date} />
+       {/* <Date dateString={postData.date} /> */}
     </Layout>
   )
 }
