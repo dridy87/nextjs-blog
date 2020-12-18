66734d1ef419ed9f6ea568c61c3dd3a6c4b2618e
DESKTOP-28M1M60\dridy <dridy@naver.com>
2020-12-19 03:45:49 +0900

    react-bootstrap add

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,10 +2,15 @@ import Head from "next/head";
 import Layout from '../../components/layout'
 import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import React, { useState } from 'react';
+import Alert from 'react-bootstrap/Alert'
 
 export default function Post({ postData }) {
-  
+  const [show, setShow] = useState(true);
+
+
   return (
+    
     <Layout>
       <Head>
         <title>{postData.title}</title>
@@ -19,9 +24,32 @@ export default function Post({ postData }) {
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
 
        <Date dateString={postData.date} />
+       <AlertDismissible/>
     </Layout>
+
   )
+
 }
+
+function AlertDismissible() {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          Change this and that and try again. Duis mollis, est non commodo
+          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+          Cras mattis consectetur purus sit amet fermentum.
+        </p>
+      </Alert>
+    );
+  }
+  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+}
+
+
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
